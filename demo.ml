9579322ebd7daea4d6a950f4f33830dc46c0fd1f
Martin Jambon <martin@mjambon.com>
2011-01-27 17:09:04 -0800

    More examples

--- demo.ml
+++ demo.ml
@@ -2,7 +2,11 @@ let print_demo_head () =
   Printf.printf "Base120       Decimal       Hexadecimal\n"
 
 let print_demo_line n =
-  Printf.printf "%-13s %-13i %-x\n" (Base120.to_string n) n n
+  Printf.printf "%-13s %-13i %s%x\n"
+    (Base120.to_string n)
+    n
+    (if n > 0 then "" else "-")
+    (abs n)
 
 let demo () =
   print_demo_head ();
@@ -12,6 +16,8 @@ let demo () =
       print_demo_line n
     done
   in
+  print_demo_line 123_456_790;
+  print_demo_line (-978_654_321);
   random 10 1_000;
   random 10 1_000_000;
   random 10 1_000_000_000;
